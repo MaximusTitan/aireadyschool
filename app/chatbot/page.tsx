c130326df3978bc6b70f522c236c8da6f4f1e29a
Yella Yohan Reddy <igebra.genai3@gmail.com>
2024-10-26 20:18:32 +0530

    Add: Code

---
+++ app/chatbot/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ChatBot = () => {
+  return (
+    <div className="">
+      <div className="">
+        <h1>Tools</h1>
+        <p>Welcome to the ChatBot Page</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBot;
